a99833eddccc395da751877cf5e0bb827278b6ff
TeamAbella060420 <teamabella@outlook.com>
2023-05-19 12:56:14 +0800

    Update index.php

--- index.php
+++ index.php
@@ -1,6 +1,31 @@
-<?php
+<!DOCTYPE html>
+<html lang="en">
 
-$firstname = 'Abraham Flynn';
-echo 'Hello ' . $firstname;
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
 
-?>
+<body>
+    <h1>
+
+        <?php
+        echo "Hello World!";
+
+        ?>
+    </h1>
+    <span>
+        <?php
+
+        $x = 2;
+        $y = 5;
+
+        echo $x . ' , ' . $y;
+        ?>
+    </span>
+    <p>My First paragraph</p>
+</body>
+
+</html>
