9e1ce12eac61977d8d3e2a40c5207b50f2b91f84
TeamAbella060420 <teamabella@outlook.com>
2023-05-19 13:01:05 +0800

    Update index.php

--- index.php
+++ index.php
@@ -10,8 +10,9 @@
 
 <body>
     <?php
+    // Comment 1
     echo '<h1>' . "Hello World!" . '</h1>';
-    $x = 2;
+    // $x = 2;
     $y = 5;
     echo '<h3>' . $x . ' , ' . $y . '</h3>';
     echo '<p>' . 'My First paragraph' . '</p>';
