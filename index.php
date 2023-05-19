465989e059700022333b98b2433bd97923227078
TeamAbella060420 <teamabella@outlook.com>
2023-05-19 17:45:08 +0800

    Update index.php

--- index.php
+++ index.php
@@ -1,22 +1,8 @@
-<!DOCTYPE html>
-<html lang="en">
+<?php
+    // Constant
 
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
+    $firstName = 'ab ab';
+    $firstName = 'love love';
 
-<body>
-    <?php
-    // Comment
-    echo '<h1>' . "Hello World!" . '</h1>';
-    // $x = 2;
-    $y = 5;
-    echo '<h3>' . $x . ' , ' . $y . '</h3>';
-    echo '<p>' . 'My First paragraph' . '</p>';
-    ?>
-</body>
-
-</html>
+    echo $firstName;
+?>
